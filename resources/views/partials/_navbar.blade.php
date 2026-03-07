908a0e6a01e5a864b606f49cc718bef843310d17
sumunardimas <sumunardimas@gmail.com>
2026-03-07 20:46:51 +0700

    Update Logo Mini

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
         <a class="navbar-brand brand-logo mr-5" href="/"><img src="{{ asset('images/logo.svg') }}" class="mr-2"
                 alt="logo" /></a>
-        <a class="navbar-brand brand-logo-mini" href="/"><img src="{{ asset('images/ukom-mini.svg') }}"
+        <a class="navbar-brand brand-logo-mini" href="/"><img src="{{ asset('images/logo-mini.svg') }}"
                 alt="logo" /></a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
--- resources/views/partials/_navbar_public.blade.php
+++ resources/views/partials/_navbar_public.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
         <a class="navbar-brand brand-logo mr-5" href="/"><img src="{{ asset('images/logo.svg') }}" class="mr-2" alt="logo"/></a>
-        <a class="navbar-brand brand-logo-mini" href="/"><img src="{{ asset('images/ukom-mini.svg') }}" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="/"><img src="{{ asset('images/logo-mini.svg') }}" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
                 
